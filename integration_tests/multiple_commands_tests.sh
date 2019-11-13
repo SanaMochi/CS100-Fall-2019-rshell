f0108d44abafe829bc4c424263ce53c89567d9fa
Michael Wdowiak <michaelwdowiak78@gmail.com>
2019-11-13 11:38:22 -0800

    Update multiple_commands_tests.sh

--- integration_tests/multiple_commands_tests.sh
+++ integration_tests/multiple_commands_tests.sh
@@ -3,5 +3,5 @@ exec 6>&1           # Link file descriptor #6 with stdout.
                     # Saves stdout.
 exec > data-file   # stdin replaced by file "data-file"
 STR="echo hello && eecho fail && echo no || echo yes && echo it works"
-../rshell $STR
+./rshell $STR
 exec 1>&6 6>&-      # Restore stdout and close file descriptor #6.
