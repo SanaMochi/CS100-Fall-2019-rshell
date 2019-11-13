82bb8e5f111a83cebeecc51b24330acc7fabb0fb
SanaMochi <44949416+SanaMochi@users.noreply.github.com>
2019-11-13 00:35:30 -0800

    Update command.cpp

--- src/command.cpp
+++ src/command.cpp
@@ -11,6 +11,38 @@ int status = 0;
 int err = 0;
 
 
+void Command::removeNextCommand(int location){
+	if(location > numOfCommands)
+		return;
+	std::vector<std::string>::iterator loc = fileNames.begin() + location;
+	fileNames.erase(loc);
+	pattern.erase(loc);
+	numOfCommands == fileNames.size();
+	int start = 0;
+	int end = 0;
+	int countQ = 0;
+	while(countQ != location){
+		for(int i = start; i < argv.size(); i++){
+			start++;
+			if(argv.at(i) == "???"){
+				countQ++;
+				break;
+			}
+			
+		}
+	}
+	//count how many commands are in between that ??? and the next one
+	for(int i = start; i < argv.size();i++){
+		if(argv.at(i) == "???")
+			break;
+		end++;
+	}
+	std::vector<std::string>::iterator v_start, v_end;
+	v_start = argv.begin() + start;
+	v_end = argv.begin() + start + end + 1;
+	argv.erase(v_start, v_end);
+}
+
 void Command::runCommand(char ** argv){
 	err = 0;
 
