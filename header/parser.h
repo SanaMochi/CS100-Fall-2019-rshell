f7d8101aecd373728442b0d8ef4392d41c22b383
SanaMochi <44949416+SanaMochi@users.noreply.github.com>
2019-11-27 00:10:05 -0800

    Update parser.h

--- header/parser.h
+++ header/parser.h
@@ -14,27 +14,10 @@ class Component;
 class Parser: public Component {
 	int status = 0;
 	int err = 0;
+	std::vector<int> opened_parens;
+	std::vector<int> closed_parens;
+	
 	public:
-	//int numOfCommands;
-	//bool exit = false;
-	//std::string command;				//used to store the whole command entered as a string
-/*	const std::string space = " ";			//
-	const std::string and_symbol = "&&";		//probably dont need these, but I thought the code would look nicer with 
-	const std::string or_symbol = "||";		//	hard coded values
-	const std::string end_symbol = ";";		//
-	std::vector<std::string> pattern; 	//stores the seperators in order
-	std::vector<std::string> commands;	//stores the initial string, but broken up by space charecters
-	std::vector<std::string> to_run;	//stores and combines the strings from commands, but broken up by '||' , '&&' , ';'
-	std::vector<std::string> fileNames; //stores only the file name of each program to be run
-	std::vector<std::string> argv;		//stores only the arguments for each program, seperates diffrent arguments by '???' to signify that the next arguments are to by run by a diffrent program
-										//e.g. fileNames.print == ls , geany , mkdir
-										//e.g argv.print == -a , ../ , ??? , file1.txt , ??? -r , folder1
-										//					^^^^^^^          ^^^^^^^^^			^^^^^^^^
-										//				run with ls			run with geany		run with mkdir
-	char ** pointer;					//stores the pointer to the arguments to run, should probably delete dynamically alocated pointers?
-	int pointerSize;					//stores how many pointers deep the 2d array is, help with deleting?
-*/	
-	//public:
 	Parser();
 	Parser(std::string s);
 	
