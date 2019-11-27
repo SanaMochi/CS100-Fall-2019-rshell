d72f35f0286881e195040d3ce9625589570f99fc
SanaMochi <44949416+SanaMochi@users.noreply.github.com>
2019-11-26 22:26:17 -0800

    Update test_literal_tests.sh

--- integration_tests/test_literal_tests.sh
+++ integration_tests/test_literal_tests.sh
@@ -1,1 +1,13 @@
 #!/bin/sh
+
+exec 6>&1           # Link file descriptor #6 with stdout.
+                    # Saves stdout.
+exec > data-file   # stdin replaced by file "data-file"
+
+if [ -e "rshell" ]
+then echo (True)
+if [ -f "header/command.h" ]
+then echo (True)
+
+exec 1>&6 6>&-      # Restore stdout and close file descriptor #6.
+exit 0
