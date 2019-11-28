050dd330879f631111f67de280929f502cf9341d
SanaMochi <44949416+SanaMochi@users.noreply.github.com>
2019-11-27 21:01:21 -0800

    Update rshell.cpp

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -23,39 +23,15 @@ int main(int argc, char *argv[]){
 		
 		while(input == "\n" || input == ""){
 			std::cout << money;
-			//std::cout << "lets go bucko";
 			std::getline(std::cin, input);
 		}
 	p.getInput(input);
-	//std::cout << "\nInput: " << input <<"\n";
 	if(input == "exit" || p.shouldIExit()){
 		return 0;
 	}
-	//p.getInput(test);
 	p.parse();				//
-	//std::cout << "input: " << p.command;
 	input = "";
-	//p.parseFileNames();		//could probably combine all 3 into 1 because they all need to be run
-	//p.parseArguments();		//
-	
-	//for testing
-	//std::cout << "\nPrint commands: "; p.printCommands();
-	//std::cout << "\nPrint arguments: ";p.printArguments();
-	//std::cout << "\nFile 1: " << p.formatFileName(0);
-	//std::cout << "\nArgument 1: " << *(p.formatArguments(0));
-	
-
-	//c->runCommand(p.formatArguments(0));
-	//std::cout << "\nPrint commands: "; p.printCommands();
-	//std::cout << "\nPrint pattern: "; p.printPattern();
-	c->runAll(p.getSize(), &p);
-	/*
-	std::cout << "\nPrint args: "; p.printArguments();
-	p.removeNextCommand(1);
-	std::cout << "\nPrint args: "; p.printArguments();
-	* */
-	//std::cout << "input: " << input;
-	//std::cout << std::endl;
+	c->runAll(p.getSize(), &p);<< input;
 	if(argc > 1)
 		return 0;
 }
