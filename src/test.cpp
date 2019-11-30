ba25b02ddf48daefcd254ad91efcfd447a8d506d
SanaMochi <44949416+SanaMochi@users.noreply.github.com>
2019-11-29 18:56:50 -0800

    Update test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -17,10 +17,9 @@ void Test::runCommand(char ** argv) {
 	struct stat buf;
 	if (stat(argv[1], &buf) == -1) {	// if no arguments for test
         	perror("stat");
-        	//return;
 		std::exit(1);
     	}
-	else if (stat(argv[1], &buf) != -1) {
+	else if (stat(argv[1], &buf) == 0) {
 		stat(argv[1], &buf);
 		if (S_ISREG(buf.st_mode) != 0) 
 			printf("(True)"); //, file_path) ;
@@ -28,16 +27,6 @@ void Test::runCommand(char ** argv) {
 			printf("(True)"); //, file_path) ;
 		else
 			printf("(False)"); //, file_path) ;
-		//	if (stat(argv[1], &buf) == 0) {
-		//		cout << "(True)" << endl;
-				//return 0;
-		//		std::exit(0);
-		//	}
-		//	else {
-		//		cout << "(False)" << endl;
-				//return 1;
-		//		std::exit(0);
-		//	}
 	}
 	else if (stat(argv[2], &buf) == 0) {
 		if (argv[1] == "-e") { 		// if file exists
@@ -45,163 +34,73 @@ void Test::runCommand(char ** argv) {
 			stat(argv[2], &buf);
 			if (S_ISREG(buf.st_mode) != 0) {
 				std::cout << "(True)" << std::endl;
-			//	printf("(True)");
 			if (S_ISDIR(buf.st_mode) != 0)
 				std::cout << "(True)" << std::endl;
-			//	printf("(True)");
 			else
 				std::cout << "(False)" << std::endl;
-			//	printf("(False)");
-		//	if (stat(argv[2], &buf) == 0) {
-		//		std::cout << "(True)" << std::endl;
-				//return 0;
-		//		std::exit(0);
-		//	}
-		//	else {
-		//		std::cout << "(False)" << std::endl;
-				//return 1;
-		//		std::exit(1);
-		//	}
 		}
 		else if (argv[1] == "-f") { 		// if is a regular file
 		//	struct stat buf;
 			stat(argv[2], &buf);
 			if (S_ISREG(buf.st_mode) != 0) 
 				std::cout << "(True)" << std::endl;
-		//		printf("(True)"); //, file_path) ;
 			else
 				std::cout << "(False)" << std::endl;
-		//		printf("(False)"); //, file_path) ;
-		//	if (stat(argv[2], &buf) == 0) {
-		//		std::cout << "(True)" << std::endl;
-				//return 0;
-		//		std::exit(0);
-		//	}
-		//	else {
-		//		std::cout << "(False)" << std::endl;
-				//return 1;
-		//		std::exit(1);
-		//	}
+
 		}
 		else if (argv[1] == "-d") {		//if is a directory
 		//	struct stat buf;
 			stat(argv[2], &buf);
 			if (S_ISDIR(buf.st_mode) != 0)
 				std::cout << "(True)" << std::endl;
-		//		printf("(True)"); //, file_path) ;
 			else
 				std::cout << "(False)" << std::endl;
-		//		printf("(False)"); //, file_path) ;
-		//	if (stat(argv[2], &buf) == 0) {
-		//		cout << "(True)" << endl;
-				//return 0;
-		//		std::exit(0);
-		//	}
-		//	else {
-		//		cout << "(False)" << endl;
-				//return 1;
-		//		std::exit(1);
-		//	}
 		}
 	}
-	
-	
-	/*
-	struct stat buf;
-
-	if (stat(argv[1], &buf) == -1) {
-        	perror("stat");
-        	return;
-    	}
-	if (stat(argv[1], &buf) != -1) { // Check the return value of stat
-    		if (S_ISREG(buf.st_mode) != 0) 
-        		printf("(True)"); //, file_path) ;
-		else if (S_ISDIR(buf.st_mode) != 0)
-        		printf("(True)"); //, file_path) ;
-    	}
-    	else {
-        	printf("(False)"); //, file_path) ;
-    	}
-	*/
 }
-/*int i = 0;
-	//assuming ONLY test executables
-	if (argv[i] != NULL) {
-//		status = system(argv[i]);
-	
-//		if (WIFSIGNALED(status) && (WTERMSIG(status) == SIGINT || WTERMSIG(status) == SIGQUIT)) {
-//			break;
-//		}
-		i++;
-		if (argv[i] == "-e" || argv[i] == "-f" || argv[i] == "-d") {
-			flags.push_back(argv[i];
-			i++;
-		}
-		else if (flags.size() == 0)
-			flags.push_back("-e");
-//		for (int j = 0; j < flags.size(); j++) {
-			if (flag.at(j) == "-e") {
-				isTrue = exists(argv[i]);
-			}else if (flag.at(j) == "-f") {
-				isTrue = isRegularFile(argv[i]);
-			}else if (flag.at(j) == "-d") {
-				isTrue = isDirectory(argv[i]);
-			}
-			if (isTrue == 0) {
-				std::cout << "(True)" << std::endl;
-			}else {
-				std::cout << "(False)" << std::endl;
-//		}
-//	i++;
-	}
-}
-*/
-
 void Test::runAll(int numOfCommands, Component* parser) {
-/*	if (commands.at(1) == "-e") { 
-		struct stat buf;
-		if (stat(argv[2], &buf) == 0) {
-			std::cout << "(True)" << std::endl;
-			return;
+	struct stat buf;
+	if (stat(argv[1], &buf) == -1) {	// if no arguments for test
+        	perror("stat");
+		std::exit(1);
+    	}
+	else if (stat(argv[1], &buf) == 0) {
+		stat(argv[1], &buf);
+		if (S_ISREG(buf.st_mode) != 0) 
+			printf("(True)"); //, file_path) ;
+		else if (S_ISDIR(buf.st_mode) != 0)
+			printf("(True)"); //, file_path) ;
+		else
+			printf("(False)"); //, file_path) ;
+	}
+	else if (stat(argv[2], &buf) == 0) {
+		if (argv[1] == "-e") { 		// if file exists
+		//	struct stat buf;
+			stat(argv[2], &buf);
+			if (S_ISREG(buf.st_mode) != 0) {
+				std::cout << "(True)" << std::endl;
+			if (S_ISDIR(buf.st_mode) != 0)
+				std::cout << "(True)" << std::endl;
+			else
+				std::cout << "(False)" << std::endl;
 		}
-		else {
-			std::cout << "(False)" << std::endl;
-			return;
+		else if (argv[1] == "-f") { 		// if is a regular file
+		//	struct stat buf;
+			stat(argv[2], &buf);
+			if (S_ISREG(buf.st_mode) != 0) 
+				std::cout << "(True)" << std::endl;
+			else
+				std::cout << "(False)" << std::endl;
+
+		}
+		else if (argv[1] == "-d") {		//if is a directory
+		//	struct stat buf;
+			stat(argv[2], &buf);
+			if (S_ISDIR(buf.st_mode) != 0)
+				std::cout << "(True)" << std::endl;
+			else
+				std::cout << "(False)" << std::endl;
 		}
 	}
-	else if (commands.at(1) == "-f") { 
-		struct stat buf;
-		if (stat(argv[2], &buf) == 0) {
-			std::cout << "(True)" << std::endl;
-			return;
-		}
-		else {
-			std::cout << "(False)" << std::endl;
-			return;
-		}
-	}
-	else if (commands.at(1) == "-d") { 
-		struct stat buf;
-		if (stat(argv[2], &buf) == 0) {
-			std::cout << "(True)" << std::endl;
-			return;
-		}
-		else {
-			std::cout << "(False)" << std::endl;
-			return;
-		}
-	}
-	else {
-		struct stat buf;
-		if (stat(argv[1], &buf) == 0) {
-			std::cout << "(True)" << std::endl;
-			return;
-		}
-		else {
-			std::cout << "(False)" << std::endl;
-			return;
-		}
-	}
-*/
 }
 
