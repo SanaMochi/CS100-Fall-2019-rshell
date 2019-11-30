d41e8a8a85e542c2378a659da067c51cc74ff103
SanaMochi <44949416+SanaMochi@users.noreply.github.com>
2019-11-29 18:14:10 -0800

    Update test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -20,77 +20,83 @@ void Test::runCommand(char ** argv) {
         	//return;
 		std::exit(1);
     	}
-	else if (argv[1] == "-e") { 		// if file exists
-	//	struct stat buf;
-		if (S_ISREG(buf.st_mode) != 0) 
-        		printf("(True)"); //, file_path) ;
-		else if (S_ISDIR(buf.st_mode) != 0)
-        		printf("(True)"); //, file_path) ;
-    		else
-        		printf("(False)"); //, file_path) ;
-	//	if (stat(argv[2], &buf) == 0) {
-	//		std::cout << "(True)" << std::endl;
-			//return 0;
-	//		std::exit(0);
-	//	}
-	//	else {
-	//		std::cout << "(False)" << std::endl;
-			//return 1;
-	//		std::exit(1);
-	//	}
-	}
-	else if (argv[1] == "-f") { 		// if is a regular file
-	//	struct stat buf;
-		if (S_ISREG(buf.st_mode) != 0) 
-        		printf("(True)"); //, file_path) ;
-    		else
-        		printf("(False)"); //, file_path) ;
-	//	if (stat(argv[2], &buf) == 0) {
-	//		std::cout << "(True)" << std::endl;
-			//return 0;
-	//		std::exit(0);
-	//	}
-	//	else {
-	//		std::cout << "(False)" << std::endl;
-			//return 1;
-	//		std::exit(1);
-	//	}
-	}
-	else if (argv[1] == "-d") {		//if is a directory
-	//	struct stat buf;
-		if (S_ISDIR(buf.st_mode) != 0)
-        		printf("(True)"); //, file_path) ;
-    		else
-        		printf("(False)"); //, file_path) ;
-	//	if (stat(argv[2], &buf) == 0) {
-	//		cout << "(True)" << endl;
-			//return 0;
-	//		std::exit(0);
-	//	}
-	//	else {
-	//		cout << "(False)" << endl;
-			//return 1;
-	//		std::exit(1);
-	//	}
-	}
-	else {					// default to check if exists
-	//	struct stat buf;
-		if (S_ISREG(buf.st_mode) != 0) 
-        		printf("(True)"); //, file_path) ;
-		else if (S_ISDIR(buf.st_mode) != 0)
-        		printf("(True)"); //, file_path) ;
-    		else
-        		printf("(False)"); //, file_path) ;
-	//	if (stat(argv[1], &buf) == 0) {
-	//		cout << "(True)" << endl;
-			//return 0;
-	//		std::exit(0);
-	//	}
-	//	else {
-	//		cout << "(False)" << endl;
-			//return 1;
-	//		std::exit(0);
-	//	}
+	else if (stat(argv[1], &buf) != -1) {
+		else if (argv[1] == "-e") { 		// if file exists
+		//	struct stat buf;
+			stat(argv[2], &buf);
+			if (S_ISREG(buf.st_mode) != 0) 
+				printf("(True)"); //, file_path) ;
+			else if (S_ISDIR(buf.st_mode) != 0)
+				printf("(True)"); //, file_path) ;
+			else
+				printf("(False)"); //, file_path) ;
+		//	if (stat(argv[2], &buf) == 0) {
+		//		std::cout << "(True)" << std::endl;
+				//return 0;
+		//		std::exit(0);
+		//	}
+		//	else {
+		//		std::cout << "(False)" << std::endl;
+				//return 1;
+		//		std::exit(1);
+		//	}
+		}
+		else if (argv[1] == "-f") { 		// if is a regular file
+		//	struct stat buf;
+			stat(argv[2], &buf);
+			if (S_ISREG(buf.st_mode) != 0) 
+				printf("(True)"); //, file_path) ;
+			else
+				printf("(False)"); //, file_path) ;
+		//	if (stat(argv[2], &buf) == 0) {
+		//		std::cout << "(True)" << std::endl;
+				//return 0;
+		//		std::exit(0);
+		//	}
+		//	else {
+		//		std::cout << "(False)" << std::endl;
+				//return 1;
+		//		std::exit(1);
+		//	}
+		}
+		else if (argv[1] == "-d") {		//if is a directory
+		//	struct stat buf;
+			stat(argv[2], &buf);
+			if (S_ISDIR(buf.st_mode) != 0)
+				printf("(True)"); //, file_path) ;
+			else
+				printf("(False)"); //, file_path) ;
+		//	if (stat(argv[2], &buf) == 0) {
+		//		cout << "(True)" << endl;
+				//return 0;
+		//		std::exit(0);
+		//	}
+		//	else {
+		//		cout << "(False)" << endl;
+				//return 1;
+		//		std::exit(1);
+		//	}
+		}
+		else {					// default to check if exists
+		//	struct stat buf;
+			stat(argv[1], &buf);
+			if (S_ISREG(buf.st_mode) != 0) 
+				printf("(True)"); //, file_path) ;
+			else if (S_ISDIR(buf.st_mode) != 0)
+				printf("(True)"); //, file_path) ;
+			else
+				printf("(False)"); //, file_path) ;
+		//	if (stat(argv[1], &buf) == 0) {
+		//		cout << "(True)" << endl;
+				//return 0;
+		//		std::exit(0);
+		//	}
+		//	else {
+		//		cout << "(False)" << endl;
+				//return 1;
+		//		std::exit(0);
+		//	}
+		}
 	}
 	
 	
