495f3969cc2b8496ea02cb3a13f28e4feee35c05
SanaMochi <44949416+SanaMochi@users.noreply.github.com>
2019-11-29 18:49:55 -0800

    Update test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -21,72 +21,13 @@ void Test::runCommand(char ** argv) {
 		std::exit(1);
     	}
 	else if (stat(argv[1], &buf) != -1) {
-/*		if (argv[1] == "-e") { 		// if file exists
-		//	struct stat buf;
-			stat(argv[2], &buf);
-			if (S_ISREG(buf.st_mode) != 0) 
-				printf("(True)"); //, file_path) ;
-			else if (S_ISDIR(buf.st_mode) != 0)
-				printf("(True)"); //, file_path) ;
-			else
-				printf("(False)"); //, file_path) ;
-		//	if (stat(argv[2], &buf) == 0) {
-		//		std::cout << "(True)" << std::endl;
-				//return 0;
-		//		std::exit(0);
-		//	}
-		//	else {
-		//		std::cout << "(False)" << std::endl;
-				//return 1;
-		//		std::exit(1);
-		//	}
-		}
-		else if (argv[1] == "-f") { 		// if is a regular file
-		//	struct stat buf;
-			stat(argv[2], &buf);
-			if (S_ISREG(buf.st_mode) != 0) 
-				printf("(True)"); //, file_path) ;
-			else
-				printf("(False)"); //, file_path) ;
-		//	if (stat(argv[2], &buf) == 0) {
-		//		std::cout << "(True)" << std::endl;
-				//return 0;
-		//		std::exit(0);
-		//	}
-		//	else {
-		//		std::cout << "(False)" << std::endl;
-				//return 1;
-		//		std::exit(1);
-		//	}
-		}
-		else if (argv[1] == "-d") {		//if is a directory
-		//	struct stat buf;
-			stat(argv[2], &buf);
-			if (S_ISDIR(buf.st_mode) != 0)
-				printf("(True)"); //, file_path) ;
-			else
-				printf("(False)"); //, file_path) ;
-		//	if (stat(argv[2], &buf) == 0) {
-		//		cout << "(True)" << endl;
-				//return 0;
-		//		std::exit(0);
-		//	}
-		//	else {
-		//		cout << "(False)" << endl;
-				//return 1;
-		//		std::exit(1);
-		//	}
-		}
-		else {					// default to check if exists
-		*/
-		//	struct stat buf;
-			stat(argv[1], &buf);
-			if (S_ISREG(buf.st_mode) != 0) 
-				printf("(True)"); //, file_path) ;
-			else if (S_ISDIR(buf.st_mode) != 0)
-				printf("(True)"); //, file_path) ;
-			else
-				printf("(False)"); //, file_path) ;
+		stat(argv[1], &buf);
+		if (S_ISREG(buf.st_mode) != 0) 
+			printf("(True)"); //, file_path) ;
+		else if (S_ISDIR(buf.st_mode) != 0)
+			printf("(True)"); //, file_path) ;
+		else
+			printf("(False)"); //, file_path) ;
 		//	if (stat(argv[1], &buf) == 0) {
 		//		cout << "(True)" << endl;
 				//return 0;
@@ -98,16 +39,19 @@ void Test::runCommand(char ** argv) {
 		//		std::exit(0);
 		//	}
 	}
-	else if (stat(argv[2], &buf) != -1) {
+	else if (stat(argv[2], &buf) == 0) {
 		if (argv[1] == "-e") { 		// if file exists
 		//	struct stat buf;
 			stat(argv[2], &buf);
-			if (S_ISREG(buf.st_mode) != 0) 
-				printf("(True)"); //, file_path) ;
-			else if (S_ISDIR(buf.st_mode) != 0)
-				printf("(True)"); //, file_path) ;
+			if (S_ISREG(buf.st_mode) != 0) {
+				std::cout << "(True)" << std::endl;
+			//	printf("(True)");
+			if (S_ISDIR(buf.st_mode) != 0)
+				std::cout << "(True)" << std::endl;
+			//	printf("(True)");
 			else
-				printf("(False)"); //, file_path) ;
+				std::cout << "(False)" << std::endl;
+			//	printf("(False)");
 		//	if (stat(argv[2], &buf) == 0) {
 		//		std::cout << "(True)" << std::endl;
 				//return 0;
@@ -123,9 +67,11 @@ void Test::runCommand(char ** argv) {
 		//	struct stat buf;
 			stat(argv[2], &buf);
 			if (S_ISREG(buf.st_mode) != 0) 
-				printf("(True)"); //, file_path) ;
+				std::cout << "(True)" << std::endl;
+		//		printf("(True)"); //, file_path) ;
 			else
-				printf("(False)"); //, file_path) ;
+				std::cout << "(False)" << std::endl;
+		//		printf("(False)"); //, file_path) ;
 		//	if (stat(argv[2], &buf) == 0) {
 		//		std::cout << "(True)" << std::endl;
 				//return 0;
@@ -141,9 +87,11 @@ void Test::runCommand(char ** argv) {
 		//	struct stat buf;
 			stat(argv[2], &buf);
 			if (S_ISDIR(buf.st_mode) != 0)
-				printf("(True)"); //, file_path) ;
+				std::cout << "(True)" << std::endl;
+		//		printf("(True)"); //, file_path) ;
 			else
-				printf("(False)"); //, file_path) ;
+				std::cout << "(False)" << std::endl;
+		//		printf("(False)"); //, file_path) ;
 		//	if (stat(argv[2], &buf) == 0) {
 		//		cout << "(True)" << endl;
 				//return 0;
