e6f9849eaa2f5c9d0357afc51254ae5605df0142
SanaMochi <44949416+SanaMochi@users.noreply.github.com>
2019-11-30 20:51:39 -0800

    Update parser.h

--- header/parser.h
+++ header/parser.h
@@ -24,7 +24,8 @@ class Parser: public Component {
 	void getInput(std::string s);		//get the input from user and store it
 	void print();						//print input, mainly used for testing
 	
-	void parse();						//parse the input string and makes a vector of words broken up by spaces, and by seperators ,that gets stored in commands and to_run respectivly
+	void parse();					//parse the input string and makes a vector of words broken up by spaces, and by seperators ,that gets stored in commands and to_run respectivly
+	void parseNoParens(string comm);				//parse the input if no parens
 	void printCommands();				//print commands, mainly used for testing
 	std::vector<std::string> getCommandsToRun(); //get to_run, mainly used for testing
 	
