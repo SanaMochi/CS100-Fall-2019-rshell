0d1cc2f8fa672a5436b5e01bdb5422573ac5a82e
SanaMochi <44949416+SanaMochi@users.noreply.github.com>
2019-12-11 17:11:06 -0800

    Update multiple_commands_tests.sh

--- integration_tests/multiple_commands_tests.sh
+++ integration_tests/multiple_commands_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-test.cpp
-g++ test.cpp -o test
+
+PATH="$PATH:.test.cpp"
+g++ integration_tests/test.cpp src/command.cpp src/component.cpp src/parser.cpp src/test.cpp -o test -std=c++11
 
 echo hello && eecho fail && echo no || echo yes && echo it works
 
