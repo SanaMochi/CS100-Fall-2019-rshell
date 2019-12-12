fb10c2539f4e31fef4723fe260fd296a62cbc091
SanaMochi <44949416+SanaMochi@users.noreply.github.com>
2019-12-11 17:11:35 -0800

    Update pipe_operator_tests.sh

--- integration_tests/pipe_operator_tests.sh
+++ integration_tests/pipe_operator_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-test.cpp
-g++ test.cpp -o test
+PATH="$PATH:.test.cpp"
+g++ integration_tests/test.cpp src/command.cpp src/component.cpp src/parser.cpp src/test.cpp -o test -std=c++11
 
 cat < names.txt | tr A-Z a-z > newOutputFile10
 
